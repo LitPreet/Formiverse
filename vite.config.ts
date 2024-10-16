38e95f8cb035dfb1ec339da930f6fd3c87439046
preet006 <preetbhardwaj146@gmail.com>
2024-10-16 16:11:30 +0530

    debugging the pwa on live

--- vite.config.ts
+++ vite.config.ts
@@ -5,63 +5,64 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
   plugins: [react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      manifest: {
-        name: 'Formiverse',
-        short_name: 'formiverse',
-        description: 'Create and manage forms effortlessly',
-        theme_color: '#ffffff',
-        background_color: '#ffffff',
-        display: 'standalone',
-        start_url: '/',
-        icons: [
-          {
-            src: '/add.png',
-            sizes: '512x512',
-            type: 'image/png',
-          },
-          {
-            src: '/bussiness-man.png',
-            sizes: '512x512',
-            type: 'image/png',
-          },
-          {
-            src: '/contact-form.png',
-            sizes: '512x512',
-            type: 'image/png',
-          },
-          {
-            src: '/feedback.png',
-            sizes: '512x512',
-            type: 'image/png',
-          },
-          {
-            src: '/formLogo.png',
-            sizes: '96x96',
-            type: 'image/png',
-          },
-          {
-            src: '/oops.png',
-            sizes: '613x407',
-            type: 'image/png',
-          },
-          {
-            src: '/party-card.png',
-            sizes: '512x512',
-            type: 'image/png',
-          },
-        ],
-        screenshots:[
-          {
-            src: '/formiversemobile.png',
-            sizes: '453x796',
-            type: 'image/png',
-          },
-      
-        ]
-      },
-    }),
+  VitePWA({
+    registerType: 'autoUpdate',
+    manifest: {
+      name: 'Formiverse',
+      short_name: 'formiverse',
+      description: 'Create and manage forms effortlessly',
+      theme_color: '#ffffff',
+      background_color: '#ffffff',
+      display: 'standalone',
+      start_url: '/',
+      icons: [
+        {
+          src: '/formLogo.png',
+          sizes: '96x96',
+          type: 'image/png',
+          purpose: 'maskable'
+        },
+        {
+          src: '/add.png',
+          sizes: '512x512',
+          type: 'image/png',
+        },
+        {
+          src: '/bussiness-man.png',
+          sizes: '512x512',
+          type: 'image/png',
+        },
+        {
+          src: '/contact-form.png',
+          sizes: '512x512',
+          type: 'image/png',
+        },
+        {
+          src: '/feedback.png',
+          sizes: '512x512',
+          type: 'image/png',
+        },
+        {
+          src: '/oops.png',
+          sizes: '613x407',
+          type: 'image/png',
+        },
+        {
+          src: '/party-card.png',
+          sizes: '512x512',
+          type: 'image/png',
+        },
+      ],
+      screenshots: [
+        {
+          src: '/formiversemobile.png',
+          sizes: '453x796',
+          type: 'image/png',
+        },
+
+      ]
+    },
+  }),
   ],
   resolve: {
     alias: {
